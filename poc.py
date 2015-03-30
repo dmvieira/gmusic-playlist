41320b53dca4daeca216af7dbe9899d4c2665540
Diogo <diogo.mvieira@labsynapse.com>
2015-03-29 22:58:08 -0300

    working related artists, but need to pay google music

--- poc.py
+++ poc.py
@@ -1,8 +1,29 @@
+import base64
+import ConfigParser
 from gmusicapi import Mobileclient
 
-email = raw_input("Your email:")
-password = raw_input("Your password:")
 
+config = ConfigParser.ConfigParser()
+config.read('config.ini')
 api = Mobileclient()
-logged_in = api.login(email, password)
-print api.get_all_songs()
+
+email = config.get('login', 'email')
+password = base64.b64decode(config.get('login', 'password'))
+
+while not api.login(email, password):
+    print "Please check your credentials"
+    email = raw_input("Your email:")
+    password = raw_input("Your password:")
+
+password = base64.b64encode(password)
+config.set('login', 'email', email)
+config.set('login', 'password', password)
+config.write(open('config.ini', 'w'))
+
+song_list = api.get_all_songs()
+for artists in song_list:
+    for artist in artists['artistId']:
+        info = api.get_artist_info(artist,
+                                   include_albums=False)
+        for related in info['related_artists']:
+            print related['name']
