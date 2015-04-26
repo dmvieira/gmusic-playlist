c6596b5f0a4b62e0648a6dd42f4659bae07d2a3b
Diogo <diogo.mvieira@labsynapse.com>
2015-04-26 17:41:06 -0300

    now doing playlist with recommendations. poc working!

--- poc.py
+++ poc.py
@@ -1,6 +1,8 @@
 import base64
 import ConfigParser
-from gmusicapi import Mobileclient
+from gmusicapi import Mobileclient, exceptions
+from math import ceil
+from random import sample
 
 
 config = ConfigParser.ConfigParser()
@@ -32,18 +34,33 @@ config.write(open('config.ini', 'w'))
 
 
 max_musics = int(max_musics)
-recommended = dict()
-song_list = api.get_all_songs()
 artists = dict()
-for song in song_list:
-    add_rate = int(song.get('rating', 0)) * int(song.get('playCount', 0))
+recommended = dict()
+liked_songs = api.get_promoted_songs()
+song_list = api.get_all_songs()
+for song in liked_songs:
     for aid in song['artistId']:
+        add_rate = 5
         artists[aid] = artists.get(aid, 0) + 1 + add_rate
+
+for song in song_list:
+    if len(artists) >= max_musics:
+        break
+    rating = int(song.get('rating', 0))
+    if rating == 0:
+        add_rate = int(song.get('playCount', 0))
+        for aid in song['artistId']:
+            artists[aid] = artists.get(aid, 0) + 1 + add_rate
 for artist in artists:
     rate = artists[artist]
-    info = api.get_artist_info(artist,
-                               include_albums=False)
-    for related in info['related_artists']:
+    try:
+        info = api.get_artist_info(artist,
+                                   include_albums=False,
+                                   max_top_tracks=0,
+                                   max_rel_artist=5)
+    except exceptions.CallFailure:
+        pass
+    for related in info.get('related_artists', []):
         aid = related['artistId']
         recommended[aid] = recommended.get(aid, 0) + rate
 
@@ -52,4 +69,14 @@ max_score = float(sum(recommended.values())) or 1.0
 for artist in recommended:
     recommended[artist] = (recommended[artist] * max_musics) / max_score
 
-print recommended, sum(recommended.values())
+playlist_id = api.create_playlist('Test me Again', 'lalala')
+songs_id = list()
+for artist in recommended:
+    info = api.get_artist_info(artist, include_albums=False,
+                               max_rel_artist=0,
+                               max_top_tracks=ceil(recommended[artist]))
+    for song in info.get('topTracks', []):
+        print len(songs_id)
+        songs_id.append(api.add_aa_track(song['storeId']))
+
+api.add_songs_to_playlist(playlist_id, sample(songs_id, max_musics))
